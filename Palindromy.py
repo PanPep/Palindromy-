8dabaec45daaa4c9d89a0b914b24ec4c55c8264e
Patryk Rozesłański <PRPaatryk9988@gmail.com>
2021-09-10 12:22:31 +0200

    Add check true palindrome

--- Palindromy.py
+++ Palindromy.py
@@ -2,4 +2,9 @@ word = input()
 if len(word)<2:
     print("Zbyt mała liczba znaków")
     exit(1)
-
+if len(word) >=2:
+    s=word.replace(" ", "")
+    if s.lower()==s.lower()[::-1]:
+        print("True")
+        exit(1)
+    
