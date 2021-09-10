d693a015ec2856eee1a574f4adc78fceb93923a3
Patryk Rozesłański <PRPaatryk9988@gmail.com>
2021-09-10 12:25:45 +0200

    Add else and title

--- Palindromy.py
+++ Palindromy.py
@@ -1,4 +1,5 @@
-word = input()
+
+word = input("Podaj słowo lub zdanie: ")
 if len(word)<2:
     print("Zbyt mała liczba znaków")
     exit(1)
@@ -7,4 +8,6 @@ if len(word) >=2:
     if s.lower()==s.lower()[::-1]:
         print("True")
         exit(1)
-    
+    else:
+        print("False")
+        exit(1)
