1553fe42c91ab919264ae135595261e6fc2c0fb1
Patryk Rozesłański <PRPaatryk9988@gmail.com>
2021-09-10 12:28:23 +0200

    Add final version

--- Palindromy.py
+++ Palindromy.py
@@ -1,1 +1,13 @@
 
+word = input("Podaj słowo lub zdanie: ")
+if len(word)<2:
+    print("Zbyt mała liczba znaków")
+    exit(1)
+if len(word) >=2:
+    s=word.replace(" ", "")
+    if s.lower()==s.lower()[::-1]:
+        print("True")
+        exit(1)
+    else:
+        print("False")
+        exit(1)
